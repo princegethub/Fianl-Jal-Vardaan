09041ba2109c6d75f067fb7ff1a0772fb64ffd14
Prince Mishra <124070878+princegethub@users.noreply.github.com>
2024-12-09 03:07:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());   
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173', 'https://fianl-jal-vardaan-frontend-iid8.onrender.com'],
   credentials: true,
 }));
 
